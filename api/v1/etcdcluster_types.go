2053f5421fd292959a3c0c9d0a2115a74edd413b
YZ775 <yuzuki725.m@gmail.com>
2024-04-17 20:11:12 +0900

    first commit

---
+++ api/v1/etcdcluster_types.go
@@ -0,0 +1,76 @@
+/*
+Copyright 2024.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// EtcdClusterSpec defines the desired state of EtcdCluster
+type EtcdClusterSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of EtcdCluster. Edit etcdcluster_types.go to remove/update
+
+	//+kubebuilder:validation:Required
+	Replicas int `json:"replicas,omitempty"`
+	//+kubebuilder:validation:Required
+	EtcdImage string `json:"etcdImage,omitempty"`
+	//+kubebuilder:validation:Required
+	StorageClassName string `json:"storageClassName,omitempty"`
+	//+kubebuilder:validation:Required
+	StorageSize string `json:"storageSize,omitempty"`
+	//+kubebuilder:validation:minItems=1
+	StorageAccessModes []string `json:"storageAccessModes,omitempty"`
+}
+
+// EtcdClusterStatus defines the observed state of EtcdCluster
+type EtcdClusterStatus struct {
+	Healthy bool `json:"healthy,omitempty"`
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+//+kubebuilder:printcolumn:name="HEALTHY",type="string",JSONPath="status.healthy"
+
+// EtcdCluster is the Schema for the etcdclusters API
+type EtcdCluster struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   EtcdClusterSpec   `json:"spec,omitempty"`
+	Status EtcdClusterStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// EtcdClusterList contains a list of EtcdCluster
+type EtcdClusterList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []EtcdCluster `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&EtcdCluster{}, &EtcdClusterList{})
+}
